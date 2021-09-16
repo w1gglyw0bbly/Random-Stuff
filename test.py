e4696fad6cc3ac7bca53a9719fdb3549519ff063
w1gglyw0bbly <ronaldeangelis@gmail.com>
2021-09-16 10:52:53 -0400

    empty square 3x3 functional
    When you click on an empty space on the board it will now reveal all spots around itself correctly, not every adjacent blank space will be revealed however

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def test(board, button, buttons, y, x):
                     break
     output = buttonClicked(button, y, x)
     if output == '0':
-        checkSurroundingMain(buttons, x, y, button)
+        checkSurrounding(buttons, y, x, button)
 
 def buttonClicked(button, y, x):
     if board.fullBoard[y][x] == 'B':
@@ -44,75 +44,55 @@ def countDisabled(buttons):
                 numDisabled += 1
     return numDisabled
 
-def checkSurrounding(buttons, y, x, text):
-    testAround = []
-    if test.UL(y, x, test.fullBoard, text) == True:
-        test.append('UL')
-    if test.U(y, x, test.fullBoard, text) == True:
-        test.append('U')
-    if test.UR(y, x, test.fullBoard, text) == True:
-        test.append('UR')
-    if test.R(y, x, test.fullBoard, text) == True:
-        test.append('R')
-    if test.BR(y, x, test.fullBoard, text) == True:
-        test.append('BR')
-    if test.B(y, x, test.fullBoard, text) == True:
-        test.append('B')
-    if test.BL(y, x, test.fullBoard, text) == True:
-        test.append('BL')
-    if test.L(y, x, test.fullBoard, text) == True:
-        test.append('L')
-    return testAround
-
-def checkSurroundingMain(buttons, y, x, b):
+def checkSurrounding(buttons, y, x, b):
     dump = 0
     
     #UL
     if y - 1 < 0 or x - 1 < 0:
         dump += 1
     else:
-        buttonClicked(buttons[y - 1][9 + (x - 9) - 1], y, x)
+        buttonClicked(buttons[y - 1][9 + (x - 9) - 1], y - 1, 9 + (x - 9) - 1)
     #U
     if y - 1 < 0:
         dump += 1
     else:
-        buttonClicked(buttons[y - 1][9 + (x - 9)], y, x)
+        buttonClicked(buttons[y - 1][9 + (x - 9)], y - 1, 9 + (x - 9))
     #UR
     if y - 1 < 0 or x + 1 > board.getRowEnd() - 1:
         dump += 1
     else:
         print('hit UR')
-        buttonClicked(buttons[y - 1][9 + (x - 9) + 1], y, x)
+        buttonClicked(buttons[y - 1][9 + (x - 9) + 1], y - 1, 9 + (x - 9) + 1)
     #R
     if x + 1 > board.getRowEnd() - 1:
         dump += 1
     else:
         print('hit R')
-        buttonClicked(buttons[y][x + 1], y, x)
+        buttonClicked(buttons[y][x + 1], y, x + 1)
     #BR
     if y + 1 > 7 or x + 1 > board.getRowEnd() - 1:
         dump += 1
     else:
         print('hit BR')
-        buttonClicked(buttons[y + 1][9 - (9 - x) + 1], y, x)
+        buttonClicked(buttons[y + 1][9 - (9 - x) + 1], y + 1, 9 - (9 - x) + 1)
     #B
     if y + 1 > 7:
         dump += 1
     else:
         print('hit B')
-        buttonClicked(buttons[y + 1][9 - (9 - x)], y, x)
+        buttonClicked(buttons[y + 1][9 - (9 - x)], y + 1, 9 - (9 - x))
     #BL
     if y + 1 > 7 or x - 1 < 0:
         dump += 1
     else:
         print('hit BL')
-        buttonClicked(buttons[y + 1][9 - (9 - x) - 1], y, x)
+        buttonClicked(buttons[y + 1][9 - (9 - x) - 1], y + 1, 9 - (9 - x) - 1)
     #L
     if x - 1 < 0:
         dump += 1
     else:
         print('hit L')
-        buttonClicked(buttons[y][x - 1], y, x)
+        buttonClicked(buttons[y][x - 1], y, x - 1)
         
         
 top = tk.Tk()
