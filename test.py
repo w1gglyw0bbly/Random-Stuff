fde3c608b7d1886ddd5c013a5ab54eec83b270c7
w1gglyw0bbly <ronaldeangelis@gmail.com>
2021-09-16 12:15:19 -0400

    Almost done exposing blank spaces
    There is an odd error, it will expose adjacent blank spaces correctly almost always except when it gets towards bottom left for some reason it throws an index out of bounds error.

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ def test(board, button, buttons, y, x):
                     break
     output = buttonClicked(button, y, x)
     if output == '0':
-        checkSurrounding(buttons, y, x, button)
+        checkSurrounding(buttons, y, x)
 
 def buttonClicked(button, y, x):
     if board.fullBoard[y][x] == 'B':
@@ -44,55 +44,84 @@ def countDisabled(buttons):
                 numDisabled += 1
     return numDisabled
 
-def checkSurrounding(buttons, y, x, b):
+def checkSurrounding(buttons, y, x):
     dump = 0
     
     #UL
     if y - 1 < 0 or x - 1 < 0:
         dump += 1
+    elif buttons[y - 1][9 + (x - 9) - 1].cget('state') == 'disabled':
+        print(board.getRowEnd())
+        #print('hit UL y: ' + str(y) + ' x: ' + str(x))
+        dump += 1
     else:
+        print('hit UL y: ' + str(y) + ' x: ' + str(x))
         buttonClicked(buttons[y - 1][9 + (x - 9) - 1], y - 1, 9 + (x - 9) - 1)
+        if buttons[y - 1][9 + (x - 9) - 1].cget('bg') == 'light grey':
+            checkSurrounding(buttons, y - 1, 9 + (x - 9) - 1) 
     #U
     if y - 1 < 0:
         dump += 1
+    elif buttons[y - 1][9 + (x - 9)].cget('state') == 'disabled':
+        dump += 1
     else:
         buttonClicked(buttons[y - 1][9 + (x - 9)], y - 1, 9 + (x - 9))
+        if buttons[y - 1][9 + (x - 9)].cget('bg') == 'light grey':
+            checkSurrounding(buttons, y - 1, 9 + (x - 9)) 
     #UR
     if y - 1 < 0 or x + 1 > board.getRowEnd() - 1:
         dump += 1
-    else:
-        print('hit UR')
-        buttonClicked(buttons[y - 1][9 + (x - 9) + 1], y - 1, 9 + (x - 9) + 1)
-    #R
-    if x + 1 > board.getRowEnd() - 1:
+    elif buttons[y - 1][9 + (x - 9) + 1].cget('state') == 'disabled':
         dump += 1
     else:
-        print('hit R')
+        buttonClicked(buttons[y - 1][9 + (x - 9) + 1], y - 1, 9 + (x - 9) + 1)
+        if buttons[y - 1][9 + (x - 9) + 1].cget('bg') == 'light grey':
+            checkSurrounding(buttons, y - 1, 9 + (x - 9) + 1) 
+    #R
+    if x + 1 >= board.getRowEnd():
+        dump += 1
+    elif buttons[y][x + 1].cget('state') == 'disabled':
+        dump += 1
+    else:
         buttonClicked(buttons[y][x + 1], y, x + 1)
+        if buttons[y][x + 1].cget('bg') == 'light grey':
+            checkSurrounding(buttons, y, x + 1) 
     #BR
     if y + 1 > 7 or x + 1 > board.getRowEnd() - 1:
         dump += 1
+    elif buttons[y + 1][9 - (9 - x) + 1].cget('state') == 'disabled':
+        dump += 1
     else:
-        print('hit BR')
         buttonClicked(buttons[y + 1][9 - (9 - x) + 1], y + 1, 9 - (9 - x) + 1)
+        if buttons[y + 1][9 - (9 - x) + 1].cget('bg') == 'light grey':
+            checkSurrounding(buttons, y + 1, 9 - (9 - x) + 1) 
     #B
     if y + 1 > 7:
         dump += 1
+    elif buttons[y + 1][9 - (9 - x)].cget('state') == 'disabled':
+        dump += 1
     else:
-        print('hit B')
         buttonClicked(buttons[y + 1][9 - (9 - x)], y + 1, 9 - (9 - x))
+        if buttons[y + 1][9 - (9 - x)].cget('bg') == 'light grey':
+            checkSurrounding(buttons, y + 1, 9 - (9 - x))
     #BL
     if y + 1 > 7 or x - 1 < 0:
         dump += 1
+    elif buttons[y + 1][9 - (9 - x) - 1].cget('state') == 'disabled':
+        dump += 1
     else:
-        print('hit BL')
         buttonClicked(buttons[y + 1][9 - (9 - x) - 1], y + 1, 9 - (9 - x) - 1)
+        if buttons[y + 1][9 - (9 - x) - 1].cget('bg') == 'light grey':
+            checkSurrounding(buttons, y + 1, 9 + (9 - x) - 1)
     #L
     if x - 1 < 0:
         dump += 1
+    elif buttons[y][x - 1].cget('state') == 'disabled':
+        dump += 1
     else:
-        print('hit L')
         buttonClicked(buttons[y][x - 1], y, x - 1)
+        if buttons[y][x -1].cget('bg') == 'light grey':
+            checkSurrounding(buttons, y, x - 1) 
         
         
 top = tk.Tk()
