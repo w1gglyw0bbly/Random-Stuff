d19d5068ecc50b49a2e45cbd60a3fefa18bcfe2f
w1gglyw0bbly <ronaldeangelis@gmail.com>
2021-09-25 21:00:40 -0400

    made the image clickable and start of anim.

--- TEST/test.js
+++ TEST/test.js
@@ -1,1 +1,9 @@
 console.log('test')
+rocks = 0
+document.getElementById('rockCounter').innerText = rocks
+
+function userClick(){
+	rocks += 1
+	document.getElementById('rockCounter').innerText = rocks
+	document.getElementById('clickImage').className = 'clickImage'
+}
