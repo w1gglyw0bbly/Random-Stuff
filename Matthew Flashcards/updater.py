7e5454afa6583c2bc7c27152fc8c23dfe77c67e0
w1gglyw0bbly <ronaldeangelis@gmail.com>
2021-09-30 10:49:36 -0400

    Success
    Created a launcher that will pull for a file and write it onto the local version of that file

--- Matthew Flashcards/updater.py
+++ Matthew Flashcards/updater.py
@@ -1,10 +1,12 @@
 import requests
 from os import getcwd
 
-url = 'https://raw.githubusercontent.com/w1gglyw0bbly/Random-Stuff/main/Matthew%20Flashcards/airplaneFlashCards.py?token=AVKQGTIVD2HTCWI7EJB744DBKXEH6'
+url = 'https://raw.githubusercontent.com/w1gglyw0bbly/Random-Stuff/main/Matthew%20Flashcards/test.py?token=AVKQGTPN5TGJM5NA7HOTFIDBKXEXK'
 directory = getcwd()
-filename = directory + 'test.py'
+filename = directory + '\\test.py'
+print(filename)
 r = requests.get(url)
 
 f = open(filename, 'w')
-f.write(r.content)
+f.write(str(r.content.decode('utf-8')))
+f.close()
