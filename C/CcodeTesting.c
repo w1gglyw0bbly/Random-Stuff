7e615a02161118f4103b66a3d6d20273b3456969
w1gglyw0bbly <ronaldeangelis@gmail.com>
2021-10-20 20:46:00 -0400

    ?

---
+++ C/CcodeTesting.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int a = 0, b = 1, c = 2, d = 3, e = 4;
+	a = b - c + d * e;
+	printf("Hello World ");
+	printf("%d ", a);
+	#define BOOL char
+	#define FALSE 0
+	#define TRUE 1
+	int foo;
+	int bar = 1;
+	printf(" %d", bar);
+
+	return 0;
+}
