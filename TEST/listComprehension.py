3653be535ad906e212836276f0c8f62474ee9b8a
w1gglyw0bbly <ronaldeangelis@gmail.com>
2021-10-22 13:02:25 -0400

    working on list/set comprehension for speed

--- TEST/listComprehension.py
+++ TEST/listComprehension.py
@@ -1,5 +1,6 @@
 def main():
     sampleList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    #newList = [expressoin for item in iterable if condition == True]
     testList = [x * x for x in sampleList]
     print(testList)
 
